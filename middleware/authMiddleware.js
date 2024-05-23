09832ca503ac2f0f32c5ed474477ccec73b35dc7
Johnson adeola <johnsamxy@gmail.com>
2024-05-23 17:38:59 +0100

    implemented application middlewares

---
+++ middleware/authMiddleware.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+const { User } = require('../model/user');
+
+const requireAuth = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  // check json web token exist and verified
+  if (token) {
+    jwt.verify(token, 'deola blog secret', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.redirect('/login');
+      } else {
+        next();
+      }
+    })
+  } else {
+    res.redirect('/login');
+  }
+}
+
+// check user
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'deola blog secret', (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        let user = User.findById(decodedToken.id);
+        res.locals.user = user;
+        next();
+      }
+    })
+  } else {
+    res.locals.user = null;
+    next();
+  }
+}
+
+module.exports = { requireAuth, checkUser };
