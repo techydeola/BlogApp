60b9ddc57d55bef848ce2e4d339aa1bf31eebde3
Johnson adeola <johnsamxy@gmail.com>
2024-05-24 20:32:10 +0100

    effected upload middleware in the blog route

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,14 +1,16 @@
 const express = require('express');
-const { blogCreate_get, allPost_get } = require('../controller/blogController');
+const { blogCreate_get, allPost_get, blogCreate_post } = require('../controller/blogController');
 const { requireAuth } = require('../middleware/authMiddleware')
+const upload = require('../middleware/uploadMiddleware');
 
 // use express router
 const router = express.Router();
 
-// create blog post router
+// get request router
 router.get('/create', requireAuth, blogCreate_get);
-
-// all blog post
 router.get('/all-post', allPost_get);
+
+// post request router
+router.post('/create', upload.single('image'), blogCreate_post);
 
 module.exports = router
