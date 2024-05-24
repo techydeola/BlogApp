16b53f24c00073560d907db611970ae05ae7e278
Johnson adeola <johnsamxy@gmail.com>
2024-05-24 10:33:40 +0100

    clean up code

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -24,13 +24,13 @@ const checkUser = (req, res, next) => {
   const token = req.cookies.jwt;
 
   if (token) {
-    jwt.verify(token, 'deola blog secret', (err, decodedToken) => {
+    jwt.verify(token, 'deola blog secret', async (err, decodedToken) => {
       if (err) {
         console.log(err.message);
         res.locals.user = null;
         next();
       } else {
-        let user = User.findById(decodedToken.id);
+        let user = await User.findById(decodedToken.id);
         res.locals.user = user;
         next();
       }
