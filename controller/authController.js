5bf2440256f6345f76bb0d64189d9d8c45896e07
Johnson adeola <johnsamxy@gmail.com>
2024-05-24 11:04:05 +0100

    fix bug

--- controller/authController.js
+++ controller/authController.js
@@ -48,10 +48,10 @@ const login_get = (req, res) => {
 
 // signup controller handler for POST request
 const signup_post = async (req, res) => {
-  const { email, password } = req.body;
+  const { firstName, lastName, email, password } = req.body;
 
   try {
-    const user = await User.create({ email, password });
+    const user = await User.create({ firstName, lastName, email, password });
     const token = createToken(user._id);
     res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
     res.status(200).json({ user: user._id });
